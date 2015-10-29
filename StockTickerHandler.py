ed043ec088f3692e26f498e4bed13c6f19802a3d
Kenny Kirtland <kgkirtland@hotmail.com>
2015-10-29 08:21:37 -0400

    Delete StockTickerHandler.py

--- StockTickerHandler.py
+++
@@ -1,9 +0,0 @@
-class StockTickerHandler(object):
-    def __init__(self, stockTickerEngine):
-        self.__stockTickerEngine = stockTickerEngine
-
-    def GetStockQuote(self, tickerSymbol):
-        return self.__stockTickerEngine.GetStockQuote(tickerSymbol)
-
-    def GetTickerSymbolsFromFile(self):
-        return self.__stockTickerEngine.GetTickerSymbolsFromFile()
